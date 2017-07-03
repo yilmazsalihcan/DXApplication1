ceef6324b2560e361c02a0f66c5b1efa5f64cbcf
yilmazsalihcan <yilmazsalihcan@hotmail.com>
2017-07-03 18:12:46 +0300

    Üye Giriş Sayfası Yapıldı.

---
+++ DXApplication1/XtraForm1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace DXApplication1
+{
+    partial class XtraForm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(XtraForm1));
+            this.txtUsername = new DevExpress.XtraEditors.TextEdit();
+            this.txtPassword = new DevExpress.XtraEditors.TextEdit();
+            this.btnGirisYap = new DevExpress.XtraEditors.SimpleButton();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            ((System.ComponentModel.ISupportInitialize)(this.txtUsername.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPassword.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(126, 91);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(100, 20);
+            this.txtUsername.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(126, 117);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // btnGirisYap
+            // 
+            this.btnGirisYap.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton1.ImageOptions.Image")));
+            this.btnGirisYap.Location = new System.Drawing.Point(124, 154);
+            this.btnGirisYap.Name = "btnGirisYap";
+            this.btnGirisYap.Size = new System.Drawing.Size(102, 41);
+            this.btnGirisYap.TabIndex = 2;
+            this.btnGirisYap.Text = "Giriş Yap";
+            this.btnGirisYap.Click += new System.EventHandler(this.btnGirisYap_Click);
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(48, 94);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(55, 13);
+            this.labelControl1.TabIndex = 3;
+            this.labelControl1.Text = "Kullanıcı Adı";
+            this.labelControl1.Click += new System.EventHandler(this.labelControl1_Click);
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Location = new System.Drawing.Point(48, 124);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(22, 13);
+            this.labelControl2.TabIndex = 4;
+            this.labelControl2.Text = "Şifre";
+            // 
+            // XtraForm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 354);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.btnGirisYap);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Name = "XtraForm1";
+            this.Text = "XtraForm1";
+            ((System.ComponentModel.ISupportInitialize)(this.txtUsername.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPassword.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.TextEdit txtUsername;
+        private DevExpress.XtraEditors.TextEdit txtPassword;
+        private DevExpress.XtraEditors.SimpleButton btnGirisYap;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+    }
+}
---
+++ DXApplication1/XtraForm1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace DXApplication1
+{
+    public partial class XtraForm1 : DevExpress.XtraEditors.XtraForm
+    {
+        public XtraForm1()
+        {
+            InitializeComponent();
+        }
+
+        private void labelControl1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        TASKMANAGEMENTEntities db = new TASKMANAGEMENTEntities();
+        public static short shortId;
+        private void btnGirisYap_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+            Users u = db.Users.FirstOrDefault(x => x.Username == username && x.Password == password);
+            if (u==null)
+            {
+                MessageBox.Show("Yanlış Kullanıcı Adı veya şifre girdiniz");
+            }
+            else
+            {
+                MessageBox.Show("Hoşgeldiniz "+u.Username);
+                if (u.RoleId==1)
+                {
+                    //Kişi Admin'dir. Admin Sayfasına yönlendir. 
+                    Form1 admin = new Form1();
+                    admin.Visible = true;
+                }
+                else
+                {
+                    //Kişi User'dır. User Sayfasına yönlendir.
+                    UserPage kullaniciSayfasi = new UserPage();
+                    kullaniciSayfasi.Visible = true;
+                }                
+            }
+        }
+    }
+}
